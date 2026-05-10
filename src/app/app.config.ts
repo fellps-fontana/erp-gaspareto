572f5883a7018ff088519ba2a9ece1b2c3e69bfb
fellps-fontana <fefe.giva13@gmail.com>
2026-05-10 17:15:25 -0300

    Melhorias na responsividade e ajustes de layout para otimizar a experiência do usuário em dispositivos móveis e tablets, incluindo alterações em paddings e flexbox.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,16 +6,27 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { environment } from '../enviroments/enviroments';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "projetosfelipe-9e458",
+      appId: "1:387862323319:web:ac0159f2fad009d2062672",
+      storageBucket: "projetosfelipe-9e458.firebasestorage.app",
+      apiKey: "AIzaSyAKeaJqPLIZrAmXQuokvaw4PAEY0q0GDYM",
+      authDomain: "projetosfelipe-9e458.firebaseapp.com",
+      messagingSenderId: "387862323319",
+      measurementId: "G-X1MN9G6SEQ"
+    })),
+    provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
   ]
 };
